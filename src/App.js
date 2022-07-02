ee95b40496b598608fd45c14a6cfbcdcb11ace10
aqdas <aqdas@gmail.com>
2022-07-02 11:40:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer.jsx"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <PathfindingVisualizer></PathfindingVisualizer>
+  </div>
+  );
+}
+
+export default App;
